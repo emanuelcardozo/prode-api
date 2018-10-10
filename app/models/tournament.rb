ced9b3285b3f6b1e64ed1749ded19520033b7f6e
maxie <maxiperezc@gmail.com>
2018-10-10 19:42:19 -0300

    Adding 3 sizes of logos for each team

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,7 +2,7 @@ class Tournament
   include Mongoid::Document
   field :name, type: String
   field :country, type: String
-  field :img, type: String
+  field :image, type: String
 
   # has_many :teams
   has_many :stages
