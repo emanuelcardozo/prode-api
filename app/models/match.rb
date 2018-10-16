48637102618efd7d83f497ed301b45f06fbb62e0
maxie <maxiperezc@gmail.com>
2018-10-16 20:28:27 -0300

    Calculating points ( model, task and controller)

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,6 +4,8 @@ class Match
   field :date, type: Time
   field :home_goals, type: Integer
   field :away_goals, type: Integer
+  field :points_recolected, type: Mongoid::Boolean, default: false
+  field :tournament_id, type: String
 
   belongs_to :home, class_name: 'Team'
   belongs_to :away, class_name: 'Team'
