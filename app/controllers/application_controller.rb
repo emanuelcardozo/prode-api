733597c2134c36675b8dfe11fbe2712f00fa835d
maxie <maxiperezc@gmail.com>
2018-10-22 18:20:24 -0300

    Adding users accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def authorize
+    current_user.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(token: params[:accessToken])
+  end
+
 end
