198079a3859c5dbbd213e110304e61a0f64141e2
maxie <maxiperezc@gmail.com>
2018-10-23 16:31:25 -0300

    Sending bets within matches

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -46,10 +46,14 @@ class TournamentsController < ApplicationController
   end
 
   def get_matches_data matches
-    matches.map do |match| {
-      id: match.id.to_s,
+    user = User.find_by(token: params[:accessToken])
+    matches.map do |match|
+      bet = match.bets.where(user_id: user.id).first
+      {id: match.id.to_s,
       home: get_team_data(match.home, match.home_goals),
       away: get_team_data(match.away, match.away_goals),
+      bet_home: bet ? bet.home_goals : nil,
+      bet_away: bet ? bet.away_goals : nil,
       date: (match.date.strftime("%d/%m/%y") if match.date),
       hour: match.hour,
       state: match.state }
