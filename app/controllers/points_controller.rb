afebd0f53d37be46a619ad049dbb0b5d270c08c0
maxie <maxiperezc@gmail.com>
2019-04-26 17:51:20 -0300

    Show all user in stage

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -12,28 +12,17 @@ class PointsController < ApplicationController
   end
 
   def stage_points
-    users = users_tournament_points(params[:id])
-    if users.empty?
-      users = User.where(:is_admin.ne => true).to_a
-      all_stage_points = users.map do |user_tournament_points|
-        {
-          user_facebook_id: user_tournament_points.facebook_id,
-          name: user_tournament_points.name,
-          alias: user_tournament_points.alias,
-          facebook_id: user_tournament_points.facebook_id,
-          points: 0
-        }
-      end
-    else
-      all_stage_points = users.map do |user_tournament_points|
-        {
-          user_facebook_id: user_tournament_points.user.facebook_id,
-          name: user_tournament_points.user.name,
-          alias: user_tournament_points.user.alias,
-          facebook_id: user_tournament_points.user.facebook_id,
-          points: get_user_stage_points(user_tournament_points, Stage.where(is_current: true).first.number_of_stage)
-        }
-      end
+    tournament_id = params[:id]
+    stage_number = Stage.where(is_current: true).first.number_of_stage
+    users = User.where(:is_admin.ne => true).to_a
+    all_stage_points = users.map do |user_tournament_points|
+      {
+        user_facebook_id: user_tournament_points.facebook_id,
+        name: user_tournament_points.name,
+        alias: user_tournament_points.alias,
+        facebook_id: user_tournament_points.facebook_id,
+        points: get_user_stage_points(user_tournament_points, tournament_id, stage_number),
+      }
     end
     render :json => all_stage_points.sort_by{|user| -user[:points]}
   end
@@ -69,10 +58,14 @@ class PointsController < ApplicationController
     Point.where(tournament_id: id).to_a
   end
 
-  def get_user_stage_points(user_points, stage_number)
+  def get_user_stage_points(user, tournament_id, stage_number)
     points=0
-    user_points.history.each do |match_points|
-      points += match_points[:points] if match_points[:stage] === stage_number
+    up = Point.where(user_id: user.id, tournament_id: tournament_id).first
+    if(up)
+      user_points = up.history.select {|h| h['stage'] === stage_number}
+      user_points.each do |match_points|
+        points += match_points[:points]
+      end
     end
     points
   end
